bb688fbf961b960589536d1dbc468de43adf361f
Dzmitry Piskunou <pinkskyinyoureyes@gmail.com>
2023-04-16 23:48:54 +0300

    done: count-cats, dream-team, mac-address

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let counter = 0
+  const cat = '^^'
+  matrix.forEach(matrixElem => {
+    matrixElem.forEach(element => {
+      if (element === cat) {
+        counter += 1
+      } 
+    })
+  });
+  return counter
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(teamArr) {
+  let teamNameArr = []
+  let teamName
+  if (Array.isArray(teamArr)) {
+    teamArr.forEach(element => {
+      if (typeof element === typeof '') {
+        teamNameArr.push(element.trim().charAt(0).toUpperCase())
+      }
+    })
+  }
+  if (teamNameArr.length === 0) {
+    teamName = false
+  } else {
+    teamNameArr.sort()
+    teamName = teamNameArr.join('')
+  }
+  return teamName
 }
 
 module.exports = {
